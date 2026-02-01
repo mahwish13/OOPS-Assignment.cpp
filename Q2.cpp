550f8635767d18bb5211837253b1a7c7e5ae0e69
mahwish13 <mahwishakhtar004@gmail.com>
2026-02-01 14:53:51 +0530

    Convert Python greeting program to C++ data type sizes

---
+++ Q2.cpp
@@ -0,0 +1,17 @@
+// program2_data_types.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Data Type Sizes:\n";
+    cout << "Size of int: " << sizeof(int) << " bytes\n";
+    cout << "Size of char: " << sizeof(char) << " bytes\n";
+    cout << "Size of float: " << sizeof(float) << " bytes\n";
+    cout << "Size of double: " << sizeof(double) << " bytes\n";
+    cout << "Size of bool: " << sizeof(bool) << " bytes\n";
+    cout << "Size of long: " << sizeof(long) << " bytes\n";
+    cout << "Size of short: " << sizeof(short) << " bytes\n";
+    
+    return 0;
+}
+
