d483ab08114de42400451142328da365ce6472c2
mahwish13 <mahwishakhtar004@gmail.com>
2026-02-01 15:23:42 +0530

    Add program to display ASCII value of a character

---
+++ Module-1/q8.cpp
@@ -0,0 +1,14 @@
+// program8_ascii_value.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    
+    cout << "Enter a character: ";
+    cin >> ch;
+    
+    cout << "ASCII value of '" << ch << "' = " << int(ch) << endl;
+    
+    return 0;
+}
