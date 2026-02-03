bc1bed231b88253e9e859d3a6ff0ad37eb99027c
mahwish13 <mahwishakhtar004@gmail.com>
2026-02-03 17:15:17 +0530

    If-else

---
+++ Module-1/q22.cpp
@@ -0,0 +1,18 @@
+// program22_if_else.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    if (num % 2 == 0) {
+        cout << num << " is even." << endl;
+    } else {
+        cout << num << " is odd." << endl;
+    }
+    
+    return 0;
+}
