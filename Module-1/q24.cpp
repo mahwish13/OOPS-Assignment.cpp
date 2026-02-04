7cd4485bb0d452c17da58afd2e3475d978577631
mahwish13 <mahwishakhtar004@gmail.com>
2026-02-04 22:17:29 +0530

    Grading system

---
+++ Module-1/q24.cpp
@@ -0,0 +1,26 @@
+// program24_grading_system.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    
+    cout << "Enter marks (0-100): ";
+    cin >> marks;
+    
+    if (marks >= 90) {
+        cout << "Grade: A+" << endl;
+    } else if (marks >= 80) {
+        cout << "Grade: A" << endl;
+    } else if (marks >= 70) {
+        cout << "Grade: B" << endl;
+    } else if (marks >= 60) {
+        cout << "Grade: C" << endl;
+    } else if (marks >= 50) {
+        cout << "Grade: D" << endl;
+    } else {
+        cout << "Grade: F (Fail)" << endl;
+    }
+    
+    return 0;
+}
