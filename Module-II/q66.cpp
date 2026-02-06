6e575e8dd2426ed7057a94084c35d201fdf0f6b1
mahwish13 <mahwishakhtar004@gmail.com>
2026-02-06 23:31:39 +0530

    Operator overloading with friend

---
+++ Module-II/q66.cpp
@@ -0,0 +1,33 @@
+// program66_friend_operator.cpp
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    double real, imag;
+    
+public:
+    Complex(double r = 0, double i = 0) : real(r), imag(i) {}
+    
+    friend Complex operator+(const Complex& c1, const Complex& c2);
+    
+    void display() {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+Complex operator+(const Complex& c1, const Complex& c2) {
+    return Complex(c1.real + c2.real, c1.imag + c2.imag);
+}
+
+int main() {
+    Complex c1(3.5, 2.5);
+    Complex c2(1.5, 4.5);
+    Complex c3 = c1 + c2;
+    
+    cout << "c1 = "; c1.display();
+    cout << "c2 = "; c2.display();
+    cout << "c3 = c1 + c2 = "; c3.display();
+    
+    return 0;
+}
