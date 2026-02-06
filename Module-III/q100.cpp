040bd9c9aed97376fda1db8895a5eeefc20ba749
mahwish13 <mahwishakhtar004@gmail.com>
2026-02-07 00:23:07 +0530

    Pure virtual function

---
+++ Module-III/q100.cpp
@@ -0,0 +1,25 @@
+// program100_pure_virtual.cpp
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void draw() = 0; // Pure virtual function
+};
+
+class Circle : public Shape {
+public:
+    void draw() {
+        cout << "Drawing Circle" << endl;
+    }
+};
+
+int main() {
+    Shape* s;
+    Circle c;
+    
+    s = &c;
+    s->draw();
+    
+    return 0;
+}
